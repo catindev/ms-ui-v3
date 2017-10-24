9cf26976ba5e11df29b85ed84a0d85eedd87a9a0
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-24 22:30:06 +0600

    SMS login

--- src/static/login/script.js
+++ src/static/login/script.js
@@ -1,34 +1,55 @@
 (function() {
+
+
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault()
+
+        infoMessage.style.display = 'none'
+        errorMessage.style.display = 'none'
         loginForm.classList.remove("shake")
 
-        const { login, password } = loginForm
-        if (!login.value || !password.value) {
+        const { login, code } = loginForm
+
+        if (!login.value && !code.value) {
             loginForm.classList.add("shake")
             setTimeout(() => loginForm.classList.remove("shake"), 1000)
+            return false
         }
 
-        const body = JSON.stringify({ login: login.value, password: password.value })
+        const body = code.value !== '' ? 
+            JSON.stringify({ code: code.value })
+            :
+            JSON.stringify({ phone: login.value })
 
-        fetch(Config.API_HOST + "/sessions", {
+        const endpoint = code.value !== '' ? '/sessions' : '/sessions/password'
+        console.log(endpoint, body)
+
+        fetch(Config.API_HOST + endpoint, {
                 method: "post",
                 headers: { "Content-type": "application/json" },
                 body
             })
             .then(response => response.json())
             .then(jsonResponse => {
-              console.logjsonResponse
                 if (jsonResponse.status !== 200) throw Error(jsonResponse.message)
                 return jsonResponse
             })
             .then(({ token }) => {
+                if (code.value === '') {                   
+                    theButton.innerText = 'Войти'                    
+                    phoneNumber.style.display = 'none'
+                    smsCode.style.display = 'block'
+                    infoMessage.style.display = 'block'
+                    return false
+                }
+
                 const expires = new Date(new Date().getTime() + 31536000000)
-                document.cookie = `msid=${token}expires=${expires}path=/`
+                document.cookie = `msid=${token};expires=${expires};path=/`
                 window.location.reload()
             })
             .catch(error => {
-                console.error('Error:',error.message)
+                errorMessage.innerText = error.message + '  😦'
+                errorMessage.style.display = 'block'
                 loginForm.classList.add("shake")
                 setTimeout(() => loginForm.classList.remove("shake"), 1000)
             })
