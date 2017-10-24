3a2f9baf4d3ddd2012ae78c7e89f01fd73f509bb
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-24 19:31:38 +0600

    fix login form shake animation

--- src/static/login/script.js
+++ src/static/login/script.js
@@ -1,14 +1,15 @@
 (function() {
     loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
+        event.preventDefault()
+        loginForm.classList.remove("shake")
 
-        const { login, password } = loginForm;
+        const { login, password } = loginForm
         if (!login.value || !password.value) {
-            loginForm.classList.add("shake");
-            setTimeout(() => loginForm.classList.remove("shake"), 1000);
+            loginForm.classList.add("shake")
+            setTimeout(() => loginForm.classList.remove("shake"), 1000)
         }
 
-        const body = JSON.stringify({ login: login.value, password: password.value });
+        const body = JSON.stringify({ login: login.value, password: password.value })
 
         fetch(Config.API_HOST + "/sessions", {
                 method: "post",
@@ -18,18 +19,18 @@
             .then(response => response.json())
             .then(jsonResponse => {
               console.logjsonResponse
-                if (jsonResponse.status !== 200) throw Error(jsonResponse.message);
-                return jsonResponse;
+                if (jsonResponse.status !== 200) throw Error(jsonResponse.message)
+                return jsonResponse
             })
             .then(({ token }) => {
-                const expires = new Date(new Date().getTime() + 31536000000);
-                document.cookie = `msid=${token};expires=${expires};path=/`;
-                window.location.reload();
+                const expires = new Date(new Date().getTime() + 31536000000)
+                document.cookie = `msid=${token}expires=${expires}path=/`
+                window.location.reload()
             })
             .catch(error => {
                 console.error('Error:',error.message)
-                loginForm.classList.add("shake");
-                setTimeout(() => loginForm.classList.remove("shake"), 1000);
-            });
-    });
-})();
+                loginForm.classList.add("shake")
+                setTimeout(() => loginForm.classList.remove("shake"), 1000)
+            })
+    })
+})()
