d6416a579ce4ee64fb9358aa0e01045e834c44e1
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-30 05:17:32 +0600

    ui fixes

--- src/index.js
+++ src/index.js
@@ -14,9 +14,10 @@ app.get('/', (request, response) => response.sendFile(__dirname + '/static/leads
 app.get('/leads/hot', (request, response) => response.sendFile(__dirname + '/static/leads/template.html'))
 app.get('/leads/hot/:id', (request, response) => response.sendFile(__dirname + '/static/profile-hot/template.html'))
 
-app.get('/leads/cold/:id', (request, response) => response.sendFile(__dirname + '/static/profile-cold/template.html'))
-app.get('/leads/cold', (request, response) => response.sendFile(__dirname + '/static/cold-leads/template.html'))
-app.get('/leads/cold/new', (request, response) => response.sendFile(__dirname + '/static/cold-new/template.html'))
+app.get('/leads/cold', (request, response) => response.sendFile(__dirname + '/static/cold-leads/template.html'));
+app.get('/leads/cold/new', (request, response) => response.sendFile(__dirname + '/static/cold-new/template.html'));
+app.get('/leads/cold/:id', (request, response) => response.sendFile(__dirname + '/static/profile-cold/template.html'));
+
 
 
 const listener = app.listen(5003, () => console.log('Started at', listener.address().port))
