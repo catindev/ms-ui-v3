76ef35e19be0d4aabec27ae8379783cf05c3490c
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-30 14:28:04 +0600

    ui fixes

--- src/static/hot-profile/script.js
+++ src/static/hot-profile/script.js
@@ -1,13 +1,24 @@
 (function() {
 
-    function template({ _id, name, trunk, phones, account: { targetQuestion } }) {
+    function template({ _id, name, trunk, phones, user, account: { targetQuestion } }) {
+        const manager = user && user.name ? `
+          <div class="label">Менеджер</div>
+          <div class="data">${ user.name }</div>
+        ` : '';
+
         return `
           <h1 class="mobilePadding">
             <a href="/leads/hot" class="backButton"></a>
             ${ name }
           </h1>
-          <h2 class="mobilePadding">${ trunk.name }</h2>
+          <h2 class="mobilePadding">${ phones.join('') }</h2>
           
+          <div class="card">
+            <div class="label">Источник</div>
+            <div class="data">${ trunk.name }</div>
+            ${ manager }
+          </div>
+
           <div class="card">
             <div class="data">
               <h3>${ targetQuestion }</h3>
