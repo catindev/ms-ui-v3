a2de22589535f832501d133f40aee4c31ee39d59
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-12-20 19:19:43 +0600

    fix funnel

--- src/static/customers/funnel-buttons.js
+++ src/static/customers/funnel-buttons.js
@@ -1,52 +1,48 @@
 function requestNextStep({ id }) {
-    return fetch(`${ Config.API_HOST }/customers/${ id }/step.down?token=${ getCookie('msid') }`, {
-            method: "put",
-            headers: { "Content-type": "application/json" }
-        })
+    return fetch(`${Config.API_HOST}/customers/${id}/step.down?token=${getCookie('msid')}`, {
+        method: "put",
+        headers: { "Content-type": "application/json" }
+    })
         .then(response => response.json())
         .then(checkResponse)
         .then(console.log)
         .catch(error => console.error('Error:', error.message));
 }
 
+function processCustomer(element) {
 
-function registerFunnelButtons() {
-    var funnelButtons = document.querySelectorAll('.funnelDownButton');
+    const nextID = element.getAttribute('to-step-id');
+    const nextStep = element.getAttribute('to-step-id') === "false" ?
+        false : document.getElementById(element.getAttribute('to-step-id'));
 
+    if (nextStep === false) return;
 
-    for (var i = 0; i < funnelButtons.length; i++) {
-        funnelButtons[i].onclick = function() {
+    const nextStepID = nextStep.getAttribute('next-step-id');
+    element.setAttribute('to-step-id', nextStepID);
+    const item = document.getElementById(element.getAttribute('item-id'));
+    item.classList.add('animateFunnelDown');
 
-            const nextID = this.getAttribute('to-step-id');
-            const nextStep = this.getAttribute('to-step-id') === "false" ?
-                false : document.getElementById(this.getAttribute('to-step-id'));
-            if (nextStep === false) return;
+    const removeItem = () => {
+        item.style.display = 'none';
+        nextStep.insertBefore(item, nextStep.firstChild);
+        item.classList.remove('animateFunnelDown');
+        if (nextStepID === 'false') {
+            item.children[2].classList.remove('funnelDownButton');
+            item.children[2].classList.add('dealButton');
+            item.children[2].setAttribute('href', '/customers/' + element.getAttribute('item-id') + '/deal');
+        }
 
-            const nextStepID = nextStep.getAttribute('next-step-id');
-            this.setAttribute('to-step-id', nextStepID);
-            const item = document.getElementById(this.getAttribute('item-id'));
-            item.classList.add('animateFunnelDown');
+        const nextStepEmpty = document.querySelectorAll(`#${nextID} .emptyList`)[0]
+        nextStepEmpty.style.display = 'none';
 
-            const removeItem = () => {
-                item.style.display = 'none';
-                nextStep.insertBefore(item, nextStep.firstChild);
-                item.classList.remove('animateFunnelDown');
-                if (nextStepID === 'false') {
-                    item.children[2].classList.remove('funnelDownButton');
-                    item.children[2].classList.add('dealButton');
-                    item.children[2].setAttribute('href', '/customers/' + this.getAttribute('item-id') + '/deal');
-                }
+        console.log({ id: element.getAttribute('item-id'), toStep: nextID })
+        document.body.removeEventListener('webkitAnimationEnd', removeItem, false);
+        document.body.removeEventListener('animationend', removeItem, false);
+        item.style.display = 'block'
+        requestNextStep({ id: element.getAttribute('item-id'), toStep: nextID })
+            .then(() => console.log('request for', element.getAttribute('item-id'), 'ok'))
+    };
 
-                const nextStepEmpty = document.querySelectorAll(`#${nextID} .emptyList`)[0]
-                nextStepEmpty.style.display = 'none';
-
-                console.log({ id: this.getAttribute('item-id'), toStep: nextID })
-                requestNextStep({ id: this.getAttribute('item-id'), toStep: nextID })
-                  .then( () => item.style.display = 'block' )
-            };
-
-            document.body.addEventListener('animationend', removeItem);
-            document.body.addEventListener('webkitAnimationEnd', removeItem);
-        };
-    }
+    document.body.addEventListener('animationend', removeItem);
+    document.body.addEventListener('webkitAnimationEnd', removeItem);
 }
--- src/static/customers/script.js
+++ src/static/customers/script.js
@@ -1,65 +1,64 @@
-(function() {
+(function () {
 
-    let funnel;
+  let funnel;
 
-    function customer({ name, info, _id,  }, next) {
-        return `
-          <div class="row lead" id="${ _id }">
-              <div class="col callbackButton" customer="${ _id }"></div>
+  function customer({ name, info, _id, }, next) {
+    return `
+          <div class="row lead" id="${ _id}">
+              <div class="col callbackButton" customer="${ _id}"></div>
               <div class="col">
-                  <a class="name" href="/customers/${ _id }">
-                    ${ name }
+                  <a class="name" href="/customers/${ _id}">
+                    ${ name}
                   </a>
                   <div class="row info wbrdr">
-                    <span>${ info }</span>
+                    <span>${ info}</span>
                 </div>
               </div>
-              <a class="col ${ next === false? 'dealButton' : 'funnelDownButton' } " 
-                   ${ next === false && `href=/customers/${_id}/deal` }
-                   item-id="${ _id }"
-                   to-step-id="${ next }"></a>
+              <a onclick="processCustomer(this)" class="col ${ next === false ? 'dealButton' : 'funnelDownButton'} " 
+                   ${ next === false && `href=/customers/${_id}/deal`}
+                   item-id="${ _id}"
+                   to-step-id="${ next}"></a>
           </div>  
         `
-    }    
+  }
 
 
-    function customers({ name, customers }, index ) {
-      const nextID = index !== funnel.length - 1? 
-        funnel[index + 1].name : false;
+  function customers({ name, customers, id }, index) {
+    const nextID = index !== funnel.length - 1 ?
+      funnel[index + 1].id : false;
 
-      const empty = !customers || customers.length === 0;
-      
-      return `
-          ${ 
-            name === 'in-progress' ? 
-              `<h1 class="pageTitle">В работе</h1>` 
-              : 
-              `<h2 class="funnelTitle">${name}</h2>` 
-          }
+    const empty = !customers || customers.length === 0;
+
+    return `
+          ${
+      name === 'in-progress' ?
+        `<h1 class="pageTitle">В работе</h1>`
+        :
+        `<h2 class="funnelTitle">${name}</h2>`
+      }
           
-          <div id="${ name }" next-step-id="${ nextID }"> 
-            <div class="emptyList" ${ !empty && 'style="display:none;"' }>
+          <div id="${ id}" next-step-id="${nextID}"> 
+            <div class="emptyList" ${ !empty && 'style="display:none;"'}>
               Нет клиентов
             </div>
-            ${ empty ? '' : (customers.map(c => customer(c, nextID) )).join('') }
+            ${ empty ? '' : (customers.map(c => customer(c, nextID))).join('')}
           </div>`
-    }
+  }
 
-    fetch(`${ Config.API_HOST }/customers/funnel?token=${ getCookie('msid') }`)
-        .then(response => response.json())
-        .then(checkResponse)
-        .then(({ items }) => {
-            funnel = items;
-            closedList.classList.remove('preloader');
+  fetch(`${Config.API_HOST}/customers/funnel?token=${getCookie('msid')}`)
+    .then(response => response.json())
+    .then(checkResponse)
+    .then(({ items }) => {
+      funnel = items;
+      closedList.classList.remove('preloader');
 
-            if (!items || items.length === 0) return closedList.innerHTML = `
+      if (!items || items.length === 0) return closedList.innerHTML = `
               <h1 class="pageTitle">В работе</h1>
               <div class="emptyList">Клиентов нет</div>
             `
 
-            closedList.innerHTML = (items.map(customers)).join('') 
-            registerFunnelButtons();  
-            registerCallbacks();             
-        })
-        .catch(error => console.error('Error:', error.message));
+      closedList.innerHTML = (items.map(customers)).join('')
+      registerCallbacks();
+    })
+    .catch(error => console.error('Error:', error.message));
 })();
