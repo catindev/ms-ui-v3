5bf6d42c5bcdcb8c3d1b93d388999e89511f14b6
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-12-26 02:54:10 +0600

    Recents fix, onboarding

--- src/static/recent-calls/script.js
+++ src/static/recent-calls/script.js
@@ -23,6 +23,44 @@
         `
     }
 
+    const fake = [
+        {
+            "_id": "5a40edacca6e854b301215be",
+            "date": "Сегодня, 26 декабря 10:03",
+            "customer": {
+                "id": "5a409ca590f4b36a4624e11b",
+                "name": "Замечательный тапир"
+            },
+            "missed": false,
+            "isCallback": true,
+            "owner": "you"
+        },
+
+        {
+            "_id": "5a40edacca6e854b301215be",
+            "date": "Сегодня, 26 декабря 09:23",
+            "customer": {
+                "id": "5a409ca590f4b36a4624e11b",
+                "name": "Утончённый ягуар"
+            },
+            "missed": false,
+            "isCallback": false,
+            "owner": "Иван"
+        },
+
+        {
+            "_id": "5a40edacca6e854b301215be",
+            "date": "Сегодня, 26 декабря 08:45",
+            "customer": {
+                "id": "5a409ca590f4b36a4624e11b",
+                "name": "Чудная акула"
+            },
+            "missed": true,
+            "isCallback": false,
+            "owner": "you"
+        }
+    ]
+
     fetch(Config.API_HOST + "/recent.calls?token=" + getCookie('msid'))
         .then(response => response.json())
         .then(checkResponse)
