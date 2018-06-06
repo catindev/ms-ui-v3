27175c87138b4160ace4a1490228d550da51cfda
MVladimir Titskiy <catindev@gmail.com>
2018-06-06 13:46:11 +0600

    client timestamp

--- src/static/coldback/script.js
+++ src/static/coldback/script.js
@@ -1,18 +1,28 @@
+var isCallbackInProgress = false;
+
 function registerCallbacks() {
+    if (isCallbackInProgress === true) return false;
+
+    isCallbackInProgress = true;
+    const requestTimestamp = new Date().getTime();
     const callButtons = document.getElementsByClassName('callbackButton');
 
-    const callHandler = function() {
+    const callHandler = function () {
         callbackLayout.style.display = 'block';
-        
+
         const customer = this.getAttribute('customer');
-        fetch(`${Config.API_HOST}/customers/${customer}/cold.call?token=${getCookie('msid')}`)
+        fetch(`${Config.API_HOST}/customers/${customer}/cold.call?token=${getCookie('msid')}&client_timestamp=${requestTimestamp}`)
             .then(response => response.json())
             .then(checkResponse)
-            .then(response => callbackLayout.style.display = 'none')
+            .then(response => {
+                isCallbackInProgress = false;
+                callbackLayout.style.display = 'none';
+            })
             .catch(error => {
+                isCallbackInProgress = false;
                 callbackLayout.style.display = 'none';
                 console.error('Ошибка. Вызов отклонён');
-            });        
+            });
     }
 
     for (var i = 0; i < callButtons.length; i++) {
